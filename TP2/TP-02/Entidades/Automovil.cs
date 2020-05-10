9f7a24dd41f85c3222e576e4cd349ac388932ab5
Juancc1001 <varvallojuanc@gmail.com>
2020-05-10 10:45:35 -0300

    TP2

---
+++ TP2/TP-02/Entidades/Automovil.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+
+namespace Entidades
+{
+    public class Automovil : Vehiculo
+    {
+        #region atributos
+
+        public enum ETipo { Monovolumen, Sedan }
+        ETipo tipo;
+
+        #endregion
+
+        #region constructores
+
+        /// <summary>
+        /// Por defecto, TIPO será Monovolumen
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="chasis"></param>
+        /// <param name="color"></param>
+        public Automovil(EMarca marca, string chasis, ConsoleColor color)
+            : base(chasis, marca, color)
+        {
+            this.tipo = ETipo.Monovolumen;
+        }
+
+        public Automovil(EMarca marca, string chasis, ConsoleColor color, ETipo tipo)
+            : this (marca, chasis, color)
+        {
+            this.tipo = tipo;
+        }
+
+        #endregion
+
+        #region propiedades
+
+        /// <summary>
+        /// Los automoviles son medianos
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Mediano;
+            }
+        }
+
+        #endregion
+
+        #region metodos
+
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("AUTOMOVIL");
+            sb.AppendLine((string)this);
+            sb.AppendLine("TAMAÑO : " + this.Tamanio.ToString());
+            sb.AppendLine("TIPO : " + this.tipo);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+
+        #endregion
+    }
+}
---
+++ TP2/TP-02/Entidades/Vehiculo.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    /// <summary>
+    /// La clase Vehiculo no deberá permitir que se instancien elementos de este tipo.
+    /// </summary>
+    public abstract class Vehiculo
+    {
+
+        #region enumerados
+        public enum EMarca
+        {
+            Chevrolet, Ford, Renault, Toyota, BMW, Honda
+        }
+        public enum ETamanio
+        {
+            Chico, Mediano, Grande
+        }
+
+        #endregion
+
+        #region atributos
+
+        EMarca marca;
+        string chasis;
+        ConsoleColor color;
+
+        #endregion
+
+        #region constructores
+
+        protected Vehiculo(string chasis, EMarca marca, ConsoleColor color)
+        {
+            this.chasis = chasis;
+            this.marca = marca;
+            this.color = color;
+        }
+
+
+        #endregion
+
+        #region propiedades
+        /// <summary>
+        /// ReadOnly: Retornará el tamaño
+        /// </summary>
+         
+
+        protected virtual ETamanio Tamanio 
+        { 
+            get { return ETamanio.Chico ; }
+        }
+
+
+        #endregion
+
+        #region metodos
+        /// <summary>
+        /// Publica todos los datos del Vehiculo.
+        /// </summary>
+        /// <returns></returns>
+        public virtual string Mostrar()
+        {
+            return (string)this;
+        }
+
+        public static explicit operator string(Vehiculo p)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("CHASIS: ");
+            sb.AppendLine(p.chasis);
+            sb.Append("MARCA: ");
+            sb.AppendLine(p.marca.ToString());
+            sb.Append("COLOR: ");
+            sb.AppendLine(p.color.ToString());
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            bool retorno = false;
+            if (obj is Vehiculo)
+            {
+                if ((Vehiculo)obj == this)
+                    retorno = true;
+            }
+            return retorno;
+        }
+
+        #endregion
+
+        #region operadores
+        /// <summary>
+        /// Dos vehiculos son iguales si comparten el mismo chasis
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static bool operator ==(Vehiculo v1, Vehiculo v2)
+        {
+            return (v1.chasis == v2.chasis);
+        }
+        /// <summary>
+        /// Dos vehiculos son distintos si su chasis es distinto
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static bool operator !=(Vehiculo v1, Vehiculo v2)
+        {
+            return (v1.chasis == v2.chasis);
+        }
+
+        #endregion
+    }
+}
